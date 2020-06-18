5f1408d8a77add95618fd216fce3f9929e46341c
mkubibaev <mkubibaev@gmail.com>
2020-06-18 15:26:35 +0600

    added Project model with migrations

--- src/webapp/models.py
+++ src/webapp/models.py
@@ -4,9 +4,11 @@ from django.db import models
 class Issue(models.Model):
     summary = models.CharField(max_length=100, verbose_name='Summary')
     description = models.TextField(max_length=2000, verbose_name='Description', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
     status = models.ForeignKey('webapp.Status', related_name='issues', on_delete=models.PROTECT, verbose_name='Status')
     type = models.ForeignKey('webapp.Type', related_name='issues', on_delete=models.PROTECT, verbose_name='Type')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    project = models.ForeignKey('webapp.Project', related_name='issues', on_delete=models.PROTECT,
+                                verbose_name='Project')
 
     def __str__(self):
         return self.summary
@@ -24,3 +26,13 @@ class Type(models.Model):
 
     def __str__(self):
         return self.label
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Title')
+    description = models.TextField(max_length=4000, verbose_name='Description', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
+
+    def __str__(self):
+        return self.title
